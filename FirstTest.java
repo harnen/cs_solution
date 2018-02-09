a4d023dc89e900b8c956da06f39cd0a0e0eb3863
Michał Król <mharnen@gmail.com>
2018-02-09 18:03:50 +0000

    test

--- FirstTest.java
+++ FirstTest.java
@@ -4,7 +4,7 @@ class FirstTest {
 
     void myFirstTest() {
         Calculator calculator = new Calculator();
-        assert (3 == calculator.add(1, 1)) : "Incorrect summation..."; 
+        assert (2 == calculator.add(1, 1)) : "Incorrect summation..."; 
     }
 
     public static void main(String[] args){
